40588e83c9e30b8cada7f082f5693a512fb753d0
Andre <andre.elias.vieira@gmail.com>
2020-08-22 14:36:08 -0300

    Inicializado as classes de request HTTP; modificado o nome do APP e o package dele

--- lib/main.dart
+++ lib/main.dart
@@ -11,29 +11,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Financia Mais',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
-
         visualDensity: VisualDensity.adaptivePlatformDensity,
-
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      //home: ,
       debugShowCheckedModeBanner: false,
+      initialRoute: '/' ,
+      routes: {
+        '/': (context) => MyHomePage(title: 'Flutter Demo Home Page'),
+        '/second': (context) => MyHomePage(title: 'Flutter Demo Home Page'), //modificar posteriormente
+      },
     );
   }
 }
 
+//APAGAR A CLASSE ABAIXO APÓS CRIAR UMA TELA INICIAL
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
