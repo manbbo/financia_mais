8a13ed20f7eb632178e09f4e85bf44b091286a2f
Piton <andre.elias.vieira@gmail.com>
2020-11-19 18:01:53 -0300

    Reformulado HEADER, adicionado FOOTER, refeito a tela de APP (principal)

--- Web/src/API/Axios.js
+++ Web/src/API/Axios.js
@@ -1,8 +1,28 @@
 import axios from 'axios';
 
-export default axios.create({
-  baseURL: `https://gateway.gr1d.io/sandbox/easycredito/v2/`,
-  headers: { "accept" : "application/json", 
-  "Api-Key" : "9495c25e-6dc9-4698-8f1a-8c56bce6e93e",
-  "Content-Type" : "application/json"  }
-});
+class AxiosInstance {
+  constructor(props) {
+    pipelineURL: ""
+    aditionalVals : ""
+    aditionalHeaders : []
+  }
+
+
+  theAxios = axios.create({
+    baseURL: `https://gateway.gr1d.io/sandbox/easycredito/v2/${pipelineURL}`,
+    headers: aditionalHeaders
+    // post
+    //{ "accept" : "application/json", 
+    //"Api-Key" : "9495c25e-6dc9-4698-8f1a-8c56bce6e93e",
+    //"Content-Type" : "application/json"  }
+  });
+
+  //axiosGet = axios.create({
+  //  baseURL: `https://gateway.gr1d.io/sandbox/easycredito/v2/process/status/${aditionalVals}`,
+  //  headers: 
+    // get
+    //{ "accept" : "application/json" }
+  //});
+}
+
+export default AxiosInstance
