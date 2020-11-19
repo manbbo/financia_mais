4a80370ba58197bc7a8f4cb979c458ea250d4587
Piton <andre.elias.vieira@gmail.com>
2020-11-19 18:08:29 -0300

    Adicionando e reformulando AXIOS

--- Web/src/API/Axios.js
+++ Web/src/API/Axios.js
@@ -11,17 +11,31 @@ class AxiosInstance {
   theAxios = axios.create({
     baseURL: `https://gateway.gr1d.io/sandbox/easycredito/v2/${pipelineURL}`,
     headers: aditionalHeaders
-    // post
+    // signup /process/signup
     //{ "accept" : "application/json", 
-    //"Api-Key" : "9495c25e-6dc9-4698-8f1a-8c56bce6e93e",
+    //"Api-Key" ou "X-Api-Key" : "9495c25e-6dc9-4698-8f1a-8c56bce6e93e",
     //"Content-Type" : "application/json"  }
   });
 
   //axiosGet = axios.create({
-  //  baseURL: `https://gateway.gr1d.io/sandbox/easycredito/v2/process/status/${aditionalVals}`,
+  //  baseURL: `https://gateway.gr1d.io/sandbox/easycredito/v2/process/status/${id}`,
   //  headers: 
     // get
-    //{ "accept" : "application/json" }
+    //{ "accept" : "application/json", "Api-Key" : "9495c25e-6dc9-4698-8f1a-8c56bce6e93e" }
+  //});
+
+  //axiosPut = axios.create({
+  //  baseURL: `https://gateway.gr1d.io/sandbox/easycredito/v2/process/document/${id}`,
+  //  headers: 
+    // get
+    //{ "accept" : "application/json", "Content-Type" : "application/json", "Api-Key" : "9495c25e-6dc9-4698-8f1a-8c56bce6e93e" }
+  //});
+
+  //axiosUpdate = axios.create({
+  //  baseURL: `https://gateway.gr1d.io/sandbox/easycredito/v2/process/proposal/${id}`,
+  //  headers: 
+    // get
+    //{ "accept" : "application/json", "Content-Type" : "application/json", "Api-Key" : "9495c25e-6dc9-4698-8f1a-8c56bce6e93e" }
   //});
 }
 
